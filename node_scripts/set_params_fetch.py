d4513e6938e213f85842d14e5059714ac80beb44
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-06-18 20:44:09 +0900

    Cease to set params of scan topic

--- node_scripts/set_params_fetch.py
+++ node_scripts/set_params_fetch.py
@@ -6,12 +6,12 @@ class SetParams():
     def __init__(self):
         rospy.on_shutdown(self.restore_params)
         self.store_params()
-        self.set_params()
+        # self.set_params()
         
     def store_params(self):
-        self.move_base_global_costmap_base_scan = rospy.get_param("/move_base/global_costmap/obstacles/base_scan/topic")
-        self.move_base_local_costmap_base_scan = rospy.get_param("/move_base/local_costmap/obstacles/base_scan/topic")
-        self.safe_teleop_base_local_costmap_base_scan = rospy.get_param("/safe_teleop_base/local_costmap/obstacles/base_scan/topic")
+        # self.move_base_global_costmap_base_scan = rospy.get_param("/move_base/global_costmap/obstacles/base_scan/topic")
+        # self.move_base_local_costmap_base_scan = rospy.get_param("/move_base/local_costmap/obstacles/base_scan/topic")
+        # self.safe_teleop_base_local_costmap_base_scan = rospy.get_param("/safe_teleop_base/local_costmap/obstacles/base_scan/topic")
         self.move_base_global_costmap_footprint = rospy.get_param("/move_base/global_costmap/footprint")
         self.move_base_local_costmap_footprint = rospy.get_param("/move_base/local_costmap/footprint")
         self.safe_teleop_base_local_costmap_footprint = rospy.get_param("/safe_teleop_base/local_costmap/footprint")
@@ -23,9 +23,9 @@ class SetParams():
         rospy.set_param("/safe_teleop_base/local_costmap/obstacles/base_scan/topic", "/base_scan_mux")
     
     def restore_params(self):
-        rospy.set_param("/move_base/global_costmap/obstacles/base_scan/topic", self.move_base_global_costmap_base_scan)
-        rospy.set_param("/move_base/local_costmap/obstacles/base_scan/topic", self.move_base_local_costmap_base_scan)
-        rospy.set_param("/safe_teleop_base/local_costmap/obstacles/base_scan/topic", self.safe_teleop_base_local_costmap_base_scan)
+        # rospy.set_param("/move_base/global_costmap/obstacles/base_scan/topic", self.move_base_global_costmap_base_scan)
+        # rospy.set_param("/move_base/local_costmap/obstacles/base_scan/topic", self.move_base_local_costmap_base_scan)
+        # rospy.set_param("/safe_teleop_base/local_costmap/obstacles/base_scan/topic", self.safe_teleop_base_local_costmap_base_scan)
         rospy.set_param("/move_base/global_costmap/footprint", self.move_base_global_costmap_footprint)
         rospy.set_param("/move_base/local_costmap/footprint", self.move_base_local_costmap_footprint)
         rospy.set_param("/safe_teleop_base/local_costmap/footprint", self.safe_teleop_base_local_costmap_footprint)
--- node_scripts/set_params_pr2.py
+++ node_scripts/set_params_pr2.py
@@ -6,15 +6,15 @@ class SetParams():
     def __init__(self):
         rospy.on_shutdown(self.restore_params)
         self.store_params()
-        self.set_params()
+        # self.set_params()
         
     def store_params(self):
-        self.move_base_node_global_costmap_base_scan = rospy.get_param("/move_base_node/global_costmap/obstacle_layer/base_scan_filtered/topic")
-        self.move_base_node_local_costmap_base_scan = rospy.get_param("/move_base_node/local_costmap/obstacle_layer/base_scan_filtered/topic")
-        self.safe_teleop_base_local_costmap_base_scan = rospy.get_param("/safe_teleop_base/local_costmap/obstacle_layer/base_scan_filtered/topic")
-        self.move_base_node_global_costmap_tilt_scan = rospy.get_param("/move_base_node/global_costmap/obstacle_layer/tilt_scan_filtered/topic")
-        self.move_base_node_local_costmap_tilt_scan = rospy.get_param("/move_base_node/local_costmap/obstacle_layer/tilt_scan_filtered/topic")
-        self.safe_teleop_base_local_costmap_tilt_scan = rospy.get_param("/safe_teleop_base/local_costmap/obstacle_layer/tilt_scan_filtered/topic")
+        # self.move_base_node_global_costmap_base_scan = rospy.get_param("/move_base_node/global_costmap/obstacle_layer/base_scan_filtered/topic")
+        # self.move_base_node_local_costmap_base_scan = rospy.get_param("/move_base_node/local_costmap/obstacle_layer/base_scan_filtered/topic")
+        # self.safe_teleop_base_local_costmap_base_scan = rospy.get_param("/safe_teleop_base/local_costmap/obstacle_layer/base_scan_filtered/topic")
+        # self.move_base_node_global_costmap_tilt_scan = rospy.get_param("/move_base_node/global_costmap/obstacle_layer/tilt_scan_filtered/topic")
+        # self.move_base_node_local_costmap_tilt_scan = rospy.get_param("/move_base_node/local_costmap/obstacle_layer/tilt_scan_filtered/topic")
+        # self.safe_teleop_base_local_costmap_tilt_scan = rospy.get_param("/safe_teleop_base/local_costmap/obstacle_layer/tilt_scan_filtered/topic")
 
         self.move_base_node_global_costmap_footprint = rospy.get_param("/move_base_node/global_costmap/footprint")
         self.move_base_node_local_costmap_footprint = rospy.get_param("/move_base_node/local_costmap/footprint")
@@ -30,12 +30,12 @@ class SetParams():
         rospy.set_param("/safe_teleop_base/local_costmap/obstacle_layer/tilt_scan_filtered/topic", "/tilt_scan_mux")
     
     def restore_params(self):
-        rospy.set_param("/move_base_node/global_costmap/obstacle_layer/base_scan_filtered/topic", self.move_base_node_global_costmap_base_scan)
-        rospy.set_param("/move_base_node/local_costmap/obstacle_layer/base_scan_filtered/topic", self.move_base_node_local_costmap_base_scan)
-        rospy.set_param("/safe_teleop_base/local_costmap/obstacle_layer/base_scan_filtered/topic", self.safe_teleop_base_local_costmap_base_scan)
-        rospy.set_param("/move_base_node/global_costmap/obstacle_layer/tilt_scan_filtered/topic", self.move_base_node_global_costmap_tilt_scan)
-        rospy.set_param("/move_base_node/local_costmap/obstacle_layer/tilt_scan_filtered/topic", self.move_base_node_local_costmap_tilt_scan)
-        rospy.set_param("/safe_teleop_base/local_costmap/obstacle_layer/tilt_scan_filtered/topic", self.safe_teleop_base_local_costmap_tilt_scan)
+        # rospy.set_param("/move_base_node/global_costmap/obstacle_layer/base_scan_filtered/topic", self.move_base_node_global_costmap_base_scan)
+        # rospy.set_param("/move_base_node/local_costmap/obstacle_layer/base_scan_filtered/topic", self.move_base_node_local_costmap_base_scan)
+        # rospy.set_param("/safe_teleop_base/local_costmap/obstacle_layer/base_scan_filtered/topic", self.safe_teleop_base_local_costmap_base_scan)
+        # rospy.set_param("/move_base_node/global_costmap/obstacle_layer/tilt_scan_filtered/topic", self.move_base_node_global_costmap_tilt_scan)
+        # rospy.set_param("/move_base_node/local_costmap/obstacle_layer/tilt_scan_filtered/topic", self.move_base_node_local_costmap_tilt_scan)
+        # rospy.set_param("/safe_teleop_base/local_costmap/obstacle_layer/tilt_scan_filtered/topic", self.safe_teleop_base_local_costmap_tilt_scan)
         rospy.set_param("/move_base_node/global_costmap/footprint", self.move_base_node_global_costmap_footprint)
         rospy.set_param("/move_base_node/local_costmap/footprint", self.move_base_node_local_costmap_footprint)
         rospy.set_param("/safe_teleop_base/local_costmap/footprint", self.safe_teleop_base_local_costmap_footprint)
