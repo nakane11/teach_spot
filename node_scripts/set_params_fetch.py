033458e33c81b0d255dffeaf9511b5cdda9ca313
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-06-20 09:32:30 +0900

    Add dynamic reconfigure client

--- node_scripts/set_params_fetch.py
+++ node_scripts/set_params_fetch.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 import rospy
+import dynamic_reconfigure.client
 
 class SetParams():
 
     def __init__(self):
         rospy.on_shutdown(self.restore_params)
         self.store_params()
+        self.clients = [dynamic_reconfigure.client.Client("/move_base/global_costmap/"),
+                        dynamic_reconfigure.client.Client("/move_base/local_costmap/"),
+                        dynamic_reconfigure.client.Client("/safe_teleop_base/local_costmap/")]
         # self.set_params()
         
     def store_params(self):
         # self.move_base_global_costmap_base_scan = rospy.get_param("/move_base/global_costmap/obstacles/base_scan/topic")
         # self.move_base_local_costmap_base_scan = rospy.get_param("/move_base/local_costmap/obstacles/base_scan/topic")
         # self.safe_teleop_base_local_costmap_base_scan = rospy.get_param("/safe_teleop_base/local_costmap/obstacles/base_scan/topic")
-        self.move_base_global_costmap_footprint = rospy.get_param("/move_base/global_costmap/footprint")
-        self.move_base_local_costmap_footprint = rospy.get_param("/move_base/local_costmap/footprint")
-        self.safe_teleop_base_local_costmap_footprint = rospy.get_param("/safe_teleop_base/local_costmap/footprint")
-    
+        self.footprints = [rospy.get_param("/move_base/global_costmap/footprint"),
+                           rospy.get_param("/move_base/local_costmap/footprint"),
+                           rospy.get_param("/safe_teleop_base/local_costmap/footprint")]    
 
     def set_params(self):
         rospy.set_param("/move_base/global_costmap/obstacles/base_scan/topic", "/base_scan_mux")
@@ -26,9 +29,8 @@ class SetParams():
         # rospy.set_param("/move_base/global_costmap/obstacles/base_scan/topic", self.move_base_global_costmap_base_scan)
         # rospy.set_param("/move_base/local_costmap/obstacles/base_scan/topic", self.move_base_local_costmap_base_scan)
         # rospy.set_param("/safe_teleop_base/local_costmap/obstacles/base_scan/topic", self.safe_teleop_base_local_costmap_base_scan)
-        rospy.set_param("/move_base/global_costmap/footprint", self.move_base_global_costmap_footprint)
-        rospy.set_param("/move_base/local_costmap/footprint", self.move_base_local_costmap_footprint)
-        rospy.set_param("/safe_teleop_base/local_costmap/footprint", self.safe_teleop_base_local_costmap_footprint)
+        for client, footprint in zip(self.clients, self.footprints):
+            client.update_configuration({"footprint": footprint})
 
 if __name__ == '__main__':
     rospy.init_node("set_params")
--- node_scripts/set_params_pr2.py
+++ node_scripts/set_params_pr2.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 import rospy
+import dynamic_reconfigure.client
 
 class SetParams():
 
     def __init__(self):
         rospy.on_shutdown(self.restore_params)
         self.store_params()
+        self.clients = [dynamic_reconfigure.client.Client("/move_base_node/global_costmap/"),
+                        dynamic_reconfigure.client.Client("/move_base_node/local_costmap/"),
+                        dynamic_reconfigure.client.Client("/safe_teleop_base/local_costmap/")]
         # self.set_params()
         
     def store_params(self):
@@ -15,11 +19,9 @@ class SetParams():
         # self.move_base_node_global_costmap_tilt_scan = rospy.get_param("/move_base_node/global_costmap/obstacle_layer/tilt_scan_filtered/topic")
         # self.move_base_node_local_costmap_tilt_scan = rospy.get_param("/move_base_node/local_costmap/obstacle_layer/tilt_scan_filtered/topic")
         # self.safe_teleop_base_local_costmap_tilt_scan = rospy.get_param("/safe_teleop_base/local_costmap/obstacle_layer/tilt_scan_filtered/topic")
-
-        self.move_base_node_global_costmap_footprint = rospy.get_param("/move_base_node/global_costmap/footprint")
-        self.move_base_node_local_costmap_footprint = rospy.get_param("/move_base_node/local_costmap/footprint")
-        self.safe_teleop_base_local_costmap_footprint = rospy.get_param("/safe_teleop_base/local_costmap/footprint")
-    
+        self.footprints = [rospy.get_param("/move_base_node/global_costmap/footprint"),
+                           rospy.get_param("/move_base_node/local_costmap/footprint"),
+                           rospy.get_param("/safe_teleop_base/local_costmap/footprint")]    
 
     def set_params(self):
         rospy.set_param("/move_base_node/global_costmap/obstacle_layer/base_scan_filtered/topic", "/base_scan_mux")
@@ -36,9 +38,8 @@ class SetParams():
         # rospy.set_param("/move_base_node/global_costmap/obstacle_layer/tilt_scan_filtered/topic", self.move_base_node_global_costmap_tilt_scan)
         # rospy.set_param("/move_base_node/local_costmap/obstacle_layer/tilt_scan_filtered/topic", self.move_base_node_local_costmap_tilt_scan)
         # rospy.set_param("/safe_teleop_base/local_costmap/obstacle_layer/tilt_scan_filtered/topic", self.safe_teleop_base_local_costmap_tilt_scan)
-        rospy.set_param("/move_base_node/global_costmap/footprint", self.move_base_node_global_costmap_footprint)
-        rospy.set_param("/move_base_node/local_costmap/footprint", self.move_base_node_local_costmap_footprint)
-        rospy.set_param("/safe_teleop_base/local_costmap/footprint", self.safe_teleop_base_local_costmap_footprint)
+        for client, footprint in zip(self.clients, self.footprints):
+            client.update_configuration({"footprint": footprint})        
 
 if __name__ == '__main__':
     rospy.init_node("set_params")
