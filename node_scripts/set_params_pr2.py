86dfa407a036f7c3cff742381843fa5146ed0ebe
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-09-05 13:14:25 +0900

    Integrate set_footprint to set_params

--- node_scripts/set_params_pr2.py
+++ node_scripts/set_params_pr2.py
@@ -6,7 +6,6 @@ import dynamic_reconfigure.client
 class SetParams():
 
     def __init__(self):
-        self.set_default_params()
         self.params = ["/move_base_node/global_costmap/obstacle_layer/base_scan_filtered/topic",
                        "/move_base_node/local_costmap/obstacle_layer/base_scan_filtered/topic",
                        "/safe_teleop_base/local_costmap/obstacle_layer/base_scan_filtered/topic",
@@ -15,6 +14,13 @@ class SetParams():
                        "/safe_teleop_base/local_costmap/obstacle_layer/tilt_scan_filtered/topic"]
         self.mux_topics = ["/base_scan_filtered_mux", "/base_scan_filtered_mux", "/base_scan_filtered_mux",
                        "/tilt_scan_filtered_mux", "/tilt_scan_filtered_mux", "/tilt_scan_filtered_mux"]
+
+        self.footprint_params = ["/move_base_node/global_costmap/footprint_topic",
+                       "/move_base_node/local_costmap/footprint_topic",
+                       "/safe_teleop_base/local_costmap/footprint_topic"]
+        self.footprint_name = "/dynamic_footprint"
+
+        # self.set_default_params()
         rospy.on_shutdown(self.restore_params)
         self.store_params()
         self.clients = [dynamic_reconfigure.client.Client("/move_base_node/global_costmap/"),
@@ -43,13 +49,9 @@ class SetParams():
         self.print_params()
 
     def print_params(self):
-        params = [("/move_base_node/global_costmap/obstacle_layer/base_scan_filtered/topic", "/base_scan_filtered"),
-                  ("/move_base_node/local_costmap/obstacle_layer/base_scan_filtered/topic", "/base_scan_filtered"),
-                  ("/safe_teleop_base/local_costmap/obstacle_layer/base_scan_filtered/topic", "/base_scan_filtered"),
-                  ("/move_base_node/global_costmap/obstacle_layer/tilt_scan_filtered/topic", "/tilt_scan_filtered/navigation"),
-                  ("/move_base_node/local_costmap/obstacle_layer/tilt_scan_filtered/topic", "/tilt_scan_filtered/navigation"),
-                  ("/safe_teleop_base/local_costmap/obstacle_layer/tilt_scan_filtered/topic", "/tilt_scan_filtered/navigation")]
-        for param, _ in params:
+        for param in self.params:
+            print("{} {}".format(param, rospy.get_param(param)))
+        for param in self.footprint_params:
             print("{} {}".format(param, rospy.get_param(param)))
 
     def store_params(self):
@@ -57,26 +59,30 @@ class SetParams():
         for param in self.params:
             self.move_base_topics.append(rospy.get_param(param))
 
-        self.footprints = [rospy.get_param("/move_base_node/global_costmap/footprint"),
-                           rospy.get_param("/move_base_node/local_costmap/footprint"),
-                           rospy.get_param("/safe_teleop_base/local_costmap/footprint")]    
-
     def set_params(self):
         rospy.loginfo("Set move_base parameters")
         for param, topic in zip(self.params, self.mux_topics):
             rospy.set_param(param, topic)
-        rosnode.kill_nodes(["/move_base_node"])
-        self.wait_node_startup()
-        self.print_params()
+        for param in self.footprint_params:
+            rospy.set_param(param, self.footprint_name)
+        self.kill_move_base()
         
     def restore_params(self):
         rospy.loginfo("Restore move_base parameters")
-        for client, footprint in zip(self.clients, self.footprints):
-            client.update_configuration({"footprint": footprint})        
         for param, topic in zip(self.params, self.move_base_topics):
             rospy.set_param(param, topic)
+        self.kill_move_base()
+
+    def delete_params(self):
+        rospy.loginfo("Delete footprint parameters")
+        for param in self.footprint_params:
+            if rospy.has_param(param):
+                rospy.delete_param(param)
+
+    def kill_move_base(self):
         rosnode.kill_nodes(["/move_base_node"])
         self.wait_node_startup()
+        rospy.loginfo("move_base_node respawn")
         self.print_params()
 
 if __name__ == '__main__':
